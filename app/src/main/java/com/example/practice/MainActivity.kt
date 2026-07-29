c1e542104e336c9f063619da55bc4b524c395cc4
Mahendra S <cartoonbox007@gmail.com>
2026-07-29 15:45:58 +0530

    New PR with empty description

---
+++ app/src/main/java/com/example/practice/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.practice
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * MainActivity - Empty PR Description Verification
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val tvInfo = findViewById<TextView>(R.id.tvInfo)
+        tvInfo.text = getString(R.string.info_text)
+    }
+}
